ecb11b057784bdacd87c7cda658d9c4bbd42d8fb
GARDI_DEV <96960296+gardi-bot@users.noreply.github.com>
2022-01-22 20:58:31 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -65,20 +65,6 @@ client.on("ready", () => {
       console.error(e);
     });
 });
-const channelid = "933026707756154925"; //id channele vc
-client.on("ready", () => {
-  const channel = client.channels.cache.get(channelid);
-  if (!channel) return console.error("The channel does not exist!");
-  channel
-    .join()
-    .then((connection) => {
-      console.log("Successfully connected.");
-    })
-    .catch((e) => {
-      console.error(e);
-    });
-});
-
 //////
 client.on("message", message => {
 if (message.content.startsWith(prefix + "about")) {
