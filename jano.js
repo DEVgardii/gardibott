edbca864622487a8f1858b53351f994cbf339cc9
GARDI_DEV <96960296+gardi-bot@users.noreply.github.com>
2022-01-24 16:03:12 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -619,7 +619,7 @@ client.on("message", message => {
         
           
           
-        return message.channel.send(`**:lock: <#${message.channel.id}> has been locked.**`);
+        return message.replay.send(`**:lock: <#${message.channel.id}> has been locked.**`);
       });
   }
 });
