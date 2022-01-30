7ba4b5176fa5d7427ef6ec87a52d042afe616058
GARDI_DEV <96960296+gardi-bot@users.noreply.github.com>
2022-01-30 14:16:41 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -31,23 +31,7 @@ client.on('message', id441 => {
 })
 
 ////////////////////
-client.on("guildCreate", guild => {
-  client.channels.cache.get("933029914024767488").send(`
-:join: **Join Server**: ${client.guilds.cache.size}
-:Servers: **Server Name**: ${guild.name}
-:owner: **Server Owner**: ${guild.owner}
-:iduser: **Server Id**: ${guild.id}
-:Users: **Member Count**: ${guild.memberCount}`);
-});
 
-client.on("guildDelete", guild => {
-  client.channels.cache.get("933029914024767488").send(`
-:left: **Lift Server**: ${client.guilds.cache.size}
-:Servers: **Server Name**: ${guild.name}
-:owner: **Server Owner**: ${guild.owner}
-:iduser: **Server Id**: ${guild.id}
-:Users: **Member Count**: ${guild.memberCount}`);
-});
 ///////////////////
 
 client.on("message", message => {
